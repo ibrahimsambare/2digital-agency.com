38b8583bc67ebf87d9fc562ad98e7c7971e581a6
ibrahimsambare <almou2112@gmail.com>
2026-03-06 04:07:09 +0100

    Update navigation.tsx

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router';
-import logo from 'logo-2digital.png';
+import logo from '../../assets/logo-2digital.png';
 import { Menu, X } from 'lucide-react';
 import { useState } from 'react';
 
